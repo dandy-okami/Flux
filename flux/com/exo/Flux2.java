1cdcd4ab745691da848c551b4e324bcbce4072ba
dandy-okami <vysconti@gmail.com>
2014-11-05 09:07:23 +0100

    flux version 1.0

--- flux/com/exo/Flux2.java
+++ flux/com/exo/Flux2.java
@@ -1,27 +1,28 @@
 package com.exo;
 
-import java.io.FileNotFoundException;
+import java.io.BufferedOutputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 
 public class Flux2 {
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		FileOutputStream fos = new FileOutputStream("exemple2.dat");
-
-		writeFlux(fos);
-		fos.close();
+		FileOutputStream fos = new FileOutputStream("C://Users//admin//Desktop//exemple2.dat");
+BufferedOutputStream bos = new BufferedOutputStream(fos);
+		writeFlux(bos);
+		bos.close();
 	}
 
-	private static void writeFlux(FileOutputStream fos) throws IOException {
+	private static void writeFlux(OutputStream fos) throws IOException {
 		byte b = 4;
 		long tp0 = System.currentTimeMillis();
 		for (int i = 0; i < 100000; i++) {
 			fos.write(b);
 		}
 		long tp1 = System.currentTimeMillis();
-		System.out.println("temps d'éxecution "+(tp1-tp0));
+		System.out.println("temps pour écrire "+(tp1-tp0));
 
 	}
 
